efddfde37a9140ee610ecd4f982be33c09d19bfd
Mikhail Timofeev <m.timofeev@ria.ru>
2025-09-03 17:17:46 +0300

    sub commands

--- tgntfy.go
+++ tgntfy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -10,26 +11,54 @@ import (
 	"strings"
 )
 
-func main() {
-	var keyAPI string
+func ReadKeyAPI() (string, error) {
 
-	// Ключи для командной строки
-	botUpdate := flag.Bool("update", false, "list telegram bot updates")
-
-	flag.Parse()
-
-	// Определяем путь
+	// Path
 	path, _ := os.Executable()
 	path = path[:strings.LastIndex(path, "/")+1]
 
-	// Читаем файл с настройками telegram
+	var byteValue []byte
+
+	// Read file with API key
 	if _, err := os.Stat(path + "/key.txt"); err == nil {
-		byteValue, err := os.ReadFile(path + "/key.txt")
+		byteValue, err = os.ReadFile(path + "/key.txt")
 		// if we os.ReadFile returns an error then handle it
 		if err != nil {
-			fmt.Println(err)
+			return "", err
 		}
-		keyAPI = string(byteValue)
+	} else {
+		return "", err
+	}
+	return string(byteValue), nil
+}
+
+func RunChatsCmd() *ChatsCmd {
+	chats_c := &ChatsCmd{
+		fs: flag.NewFlagSet("chats", flag.ContinueOnError),
+	}
+
+	chats_c.fs.Bool("verbose", false, "verbose chat list updates of telegram bot")
+
+	return chats_c
+}
+
+type ChatsCmd struct {
+	fs *flag.FlagSet
+}
+
+func (g *ChatsCmd) Name() string {
+	return g.fs.Name()
+}
+
+func (g *ChatsCmd) Init(args []string) error {
+	return g.fs.Parse(args)
+}
+
+func (g *ChatsCmd) Run() error {
+	// Read API key from file
+	keyAPI, err := ReadKeyAPI()
+	if err != nil {
+		return err
 	}
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -37,20 +66,53 @@ func main() {
 	// Send message to Telegram
 	client := &http.Client{}
 
-	if *botUpdate {
-		url := "https://api.telegram.org/bot" + keyAPI + "/getUpdates"
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			fmt.Printf("Cannot create new request  %s, error: %v\n", url, err)
-		}
-		// Отправляем запрос
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("Error with GET request: %v\n", err)
-		}
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		fmt.Print(body)
+	url := "https://api.telegram.org/bot" + keyAPI + "/getUpdates"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("Cannot create new request  %s, error: %v\n", url, err)
+	}
+	// Send request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error with GET request: %v\n", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	return nil
+}
+
+type Runner interface {
+	Init([]string) error
+	Run() error
+	Name() string
+}
+
+func root(args []string) error {
+	if len(args) < 1 {
+		return errors.New("You must pass a sub-command")
 	}
 
+	cmds := []Runner{
+		RunChatsCmd(),
+	}
+
+	subcommand := os.Args[1]
+
+	for _, cmd := range cmds {
+		if cmd.Name() == subcommand {
+			cmd.Init(os.Args[2:])
+			return cmd.Run()
+		}
+	}
+
+	return fmt.Errorf("Unknown subcommand: %s", subcommand)
+}
+
+func main() {
+	if err := root(os.Args[1:]); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
